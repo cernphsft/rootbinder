aba1f2fee46a889a458156a6bda64464af293021
Danilo Piparo <danilo.piparo@cern.ch>
2016-01-02 23:08:43 +0100

    Rename sources in data

---
+++ data/myLibrary.cc
@@ -0,0 +1,16 @@
+#include "myLibrary.h"
+
+A::A()
+{
+   std::cout << "This is the constructor of A" << std::endl;
+}
+
+A::~A()
+{
+   std::cout << "This is the destructor of A" << std::endl;
+}
+
+int CountCharacters(const std::string s)
+{
+   return s.size();
+}
